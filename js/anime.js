6d0ef5075be710ddeaffe9d7c2968e16a8efe287
heimish902 <heimish902@gmail.com>
2022-02-24 15:03:29 +0900

    Update anime.js

--- js/anime.js
+++ js/anime.js
@@ -1,32 +1,9 @@
-function startVivus(a, b) {
-  const scrollTop = window.scrollY;
-  const section = document.querySelector(a);
-  const top = window.pageYOffset + section.getBoundingClientRect().top;
-  if (scrollTop > top - window.innerHeight) {
-    b.play();
-  } else {
-    b.reset();
-  }
-}
-
+const menu = document.querySelector('.menu_btn');
+const bar = document.querySelectorAll('.bar');
+const nav = document.querySelector('nav');
+const bgBar = nav.querySelectorAll('.bg');
+const li = nav.querySelectorAll('li');
 const progress = document.querySelectorAll('.progress_bar');
-
-function showCont() {
-  const scrollTop = window.scrollY;
-  const container = document.querySelector('.ability');
-  const top = window.pageYOffset + container.getBoundingClientRect().top;
-
-  if (scrollTop > top - window.innerHeight / 1.3) {
-    progress.forEach((el) => {
-      let text = el.lastElementChild.innerHTML;
-      el.style.width = text;
-    });
-  } else {
-    progress.forEach((el) => {
-      el.style.width = '0%';
-    });
-  }
-}
 
 /* vivus */
 let svg1 = new Vivus('svg1', {
@@ -47,10 +24,50 @@ let svg3 = new Vivus('svg3', {
   file: '../image/707magnifyingglasstiltedleft_100258.svg',
 });
 
-const menu = document.querySelector('.menu_btn');
-const bar = document.querySelectorAll('.bar');
-const nav = document.querySelector('nav');
+/* typeit */
+document.addEventListener('DOMContentLoaded', function () {
+  new TypeIt('.typeit', {
+    speed: 150,
+  })
+    .type('Friendly')
+    .delete()
+    .type('Independent')
+    .delete()
+    .type('Continuous .')
+    .go();
+});
 
+// svg animation
+function startVivus(a, b) {
+  const scrollTop = window.scrollY;
+  const section = document.querySelector(a);
+  const top = window.pageYOffset + section.getBoundingClientRect().top;
+  if (scrollTop > top - window.innerHeight) {
+    b.play();
+  } else {
+    b.reset();
+  }
+}
+
+// skill animation
+function showCont() {
+  const scrollTop = window.scrollY;
+  const container = document.querySelector('.ability');
+  const top = window.pageYOffset + container.getBoundingClientRect().top;
+
+  if (scrollTop > top - window.innerHeight / 1.3) {
+    progress.forEach((el) => {
+      let text = el.lastElementChild.innerHTML;
+      el.style.width = text;
+    });
+  } else {
+    progress.forEach((el) => {
+      el.style.width = '0%';
+    });
+  }
+}
+
+// project animation
 function animate(a) {
   const A = document.querySelector(a);
   const B = A.querySelectorAll('div');
@@ -65,15 +82,25 @@ function animate(a) {
   });
 }
 
+// nav link click event
+li.forEach((el) => {
+  el.addEventListener('click', () => {
+    nav.style.display = 'none';
+    menu.classList.remove('close');
+    bar.forEach((el) => {
+      el.classList.remove('change');
+    });
+  });
+});
+
+// menu button click event
 menu.addEventListener('click', function () {
   bar.forEach((el) => {
     el.classList.toggle('change');
     menu.classList.toggle('close');
   });
 
-  const bgBar = nav.querySelectorAll('.bg');
-  const li = nav.querySelectorAll('li');
-
+  // nav background animation
   bgBar.forEach((bg, i) => {
     if (menu.classList.contains('close')) {
       nav.style.display = 'block';
@@ -95,13 +122,14 @@ menu.addEventListener('click', function () {
   });
 });
 
+// scroll animation
 window.addEventListener('scroll', () => {
   startVivus('#svg1', svg1);
   startVivus('#svg2', svg2);
   startVivus('#svg3', svg3);
+
   showCont();
 
-  // animate
   animate('.converse');
   animate('.gshock');
   animate('.startup');
